754494d54096b8a91da0ba3674b71087afe30f66
Trollerr. <42436350+ricecracker12@users.noreply.github.com>
2024-10-04 23:14:49 +0700

    8

---
+++ buoi8/8.3.js
@@ -0,0 +1,17 @@
+let soNgauNhien
+soNgauNhien = Math.floor(Math.random() * 10) + 1;
+
+function doanSo() {
+    let soNguoiDungNhap = parseInt(document.getElementById("soNhap").value);
+
+    if (isNaN(soNguoiDungNhap)) {
+        document.getElementById("ketQua").innerHTML = "Invalid input";
+        return;
+    }
+
+    if (soNguoiDungNhap === soNgauNhien) {
+        document.getElementById("ketQua").innerHTML = "juan r!";
+    } else {
+        document.getElementById("ketQua").innerHTML = "sai r.";
+    }
+}
